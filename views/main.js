fd1f06faeea8fecc76a755e3dcbabee3deb60dd6
sril <srilq.git@gmail.com>
2017-08-12 12:45:04 +0100

    new ticker content

--- views/main.js
+++ views/main.js
@@ -2,19 +2,24 @@ var html = require('choo/html')
 
 var TITLE = 'stef'
 var INTRO = 'Stef is a 22yo computer science graduate (BSc), web developer and Unity developer, former designer for publications + marketing.'
-var TICKER_ITEMS = [
-    { id: 'winningsolutions', url: 'http://www.winningsolutions.co.uk' },
+var TICKER = [
     { id: 'lifeisstrange', url: 'http://www.feralinteractive.com/en/games/lifeisstrange/' },
+    { id: 'companyofheroes2', url: 'http://www.feralinteractive.com/en/games/companyofheroes2/' },
+    { id: 'madmax', url: 'http://www.feralinteractive.com/en/games/madmax/' },
+    { id: 'winningsolutions', url: 'http://www.winningsolutions.co.uk' },
     { id: 'legomarvelsavengers', url: 'http://www.feralinteractive.com/en/mac-games/legomarvelsavengers/' },
+    { id: 'alienisolation', url: 'http://www.feralinteractive.com/en/games/alienisolation/' },
     { id: 'dirtrally', url: 'http://www.feralinteractive.com/en/linux-games/dirtrally/' },
-    { id: 'alienisolation', url: 'http://www.feralinteractive.com/en/games/alienisolation/' }
+    { id: 'co600', url: 'https://github.com/srilq/co600-corpus' },
+    { id: 'cab1' },
+    { id: 'cab2' }
 ]
 
 module.exports = view
 
 function view (state, emit) {
   if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
-  TICKER_ITEMS = TICKER_ITEMS.map(function (item) {
+  TICKER = TICKER.map(function (item) {
     item.image = state.staticBase + '/assets/' + item.id + '.jpg'
     return item
   })
@@ -48,20 +53,26 @@ function Ticker () {
       <div class="ticker ticker--video">
         <div class="ticker__wrapper">
           <div class="ticker__rot">
-            <ul class="ticker__list">${tickerVideos()}${tickerVideos()}</ul>
+            <ul class="ticker__list">${items(TICKER)}${items(TICKER)}</ul>
           </div>
         </div>
       </div>
     </div>
   `
 
-  function tickerVideos () {
-    return TICKER_ITEMS.map(tickerVideo)
-  }
+  function items (ticker) {
+    return ticker.map(item)
 
-  function tickerVideo (item) {
-    return html`
-      <li class="ticker__item ticker__item--image"><a href="${item.url}"><div class="ticker__media ticker__media--image image"><img src="${item.image}"></div></a></li>
-    `
+    function item (item) {
+      var li = html`<li class="ticker__item ticker__item--image"></li>`
+      var imageContainer = li
+      var image = html`<div class="ticker__media ticker__media--image image"><img src="${item.image}"></div>`
+      if (item.url) {
+        imageContainer = html`<a href="${item.url}"></a>`
+        li.appendChild(imageContainer)
+      }
+      imageContainer.appendChild(image)
+      return li
+    }
   }
 }
